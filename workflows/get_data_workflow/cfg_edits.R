6085d806253a610ae57d426cbce5851ae7b86f91
Kyle Oliveira <koliveira@bigelow.org>
2024-12-04 20:09:56 -0500

    curated models

--- workflows/get_data_workflow/cfg_edits.R
+++ workflows/get_data_workflow/cfg_edits.R
@@ -1,9 +1,7 @@
-files = list.files(path = "workflows/forecast_workflow", pattern = glob2rx("v01.2*.yaml"), full.names = TRUE)
-extra = list(fall_fish_mon = c(12, 1, 2, 3, 4, 5),
-             spring_fish_mon = c(6, 7, 8, 9, 10, 11),
-             which_fish = "COMBO")
+files = list.files(path = "workflows/get_data_workflow", pattern = glob2rx("v01.*.yaml"), full.names = TRUE)
+extra = list(inat_file = "inaturalist/observations-507834.csv")
 cfgs = append_config(files, extra, write = TRUE)
 
-files = list.files(path = "workflows/forecast_workflow", pattern = glob2rx("v01.070*.yaml"), full.names = TRUE)
+files = list.files(path = "workflows/modeling_workflow", pattern = glob2rx("v01.**2.*.yaml"), full.names = TRUE)
 cfg = verify_version(files, write = TRUE)
 cfg
--- workflows/modeling_workflow/maxent_modeling.R
+++ workflows/modeling_workflow/maxent_modeling.R
@@ -27,7 +27,7 @@ args = argparser::arg_parser("maxent modeling for white shark obs",
                              hide.opts = TRUE) |>
   argparser::add_argument(arg = "--config",
                           type = "character",
-                          default = "/mnt/s1/projects/ecocast/projects/koliveira/subprojects/carcharodon/workflows/modeling_workflow/v02.000.12.yaml",
+                          default = "/mnt/s1/projects/ecocast/projects/koliveira/subprojects/carcharodon/workflows/modeling_workflow/v01.002.08.yaml",
                           help = "the name of the configuration file") |>
   argparser::parse_args()
 
